6896a9948bcd015835693b04a0773390566b5f7b
wessam aftab <wisamkayani360@gmail.com>
2023-07-29 22:38:40 +0500

    ok

--- 2-personalmessage/pm.ts
+++
@@ -1,2 +0,0 @@
-var person_name="wessam aftab";
-console.log("Hello!",person_name,",how are you ?");
---
+++ personalmessage/pm.js
@@ -0,0 +1,2 @@
+var person_name = "wessam aftab";
+console.log("Hello! {persnal_name},how are you ?
