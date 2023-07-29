b9a3cafd6def6a8537b38198c05dcd3d650bb788
wessam aftab <wisamkayani360@gmail.com>
2023-07-29 22:32:14 +0500

    add

---
+++ 2-personalmessage/pm.ts
@@ -0,0 +1,2 @@
+var person_name="wessam aftab";
+console.log("Hello!",person_name,",how are you ?");
---
+++ 5-namecases/nc.ts
@@ -0,0 +1,3 @@
+var nc = "wessam aftab";
+let t1= nc.toUpperCase(),t2=nc.toLowerCase(),t3=nc.split().;
+console.log(t1,t2);
