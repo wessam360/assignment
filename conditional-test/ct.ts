27e4b3e1f53f97569998f2825470bed6df43fb1a
wessam aftab <wisamkayani360@gmail.com>
2023-07-30 19:15:00 +0500

    ok

--- conditional-test/ct.ts
+++ conditional-test/ct.ts
@@ -1,2 +1,11 @@
-let car=`honda`;
-console.log("car==`honda`?`ture`:`false`")
+let car=`Honda`,clas=`12`,rollno=420,height=12,color=`blue`;
+let body=`flat`,diseases=false,size=`11`,bones=206,ribs=24; 
+// 5 false conditons
+console.log(car==car.toLowerCase(),clas===12,rollno!=420,height!=12,color==`green`)
+// 5 true conditons
+console.log(body!=`ok`,diseases==false,size==11,bones==206,ribs===24);
+// clas and size only work for javascript not typescript.
+
+// more condtion test program #24:
+let arr=`   aliahjed`;
+console.log(arr.trim())
