b31d95296c8432580e8b3aa8bc63fcd2d654274f
wessam aftab <wisamkayani360@gmail.com>
2023-08-05 00:24:05 +0500

    ok

---
+++ ordinal-numbers/on.js
@@ -0,0 +1,10 @@
+"use strict";
+let ordinal_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+ordinal_numbers.forEach((value) => {
+    if (value == 1 || value == 2)
+        console.log(`${value}st`);
+    else if (value == 3)
+        console.log(`${value}rd`);
+    else
+        console.log(`${value}th`);
+});
